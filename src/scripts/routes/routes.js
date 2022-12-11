387b68d6ff58d2ce66aaa5f29d0cdc5a0e9bb98c
MUHAMMAD IHSANUDIN SYAIFULLAH <111124211+IhsanudinS@users.noreply.github.com>
2022-12-11 09:46:39 +0700

    Add donasi-page routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,12 @@
 import volunteer from '../pages/volunteer-page';
 import inklusif from '../pages/inklusif-page';
 import school from '../pages/school-page';
+import donasi from '../pages/donasi-page';
 
 const routes = {
   '/inklusif': inklusif,
   '/volunteer': volunteer,
+  '/info-donasi': donasi,
   '/school': school,
 };
 
